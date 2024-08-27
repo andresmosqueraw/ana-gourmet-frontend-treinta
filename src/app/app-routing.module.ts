24f8142b0da8e1a6c5ffe6d077a35040f9876305
cbazurto <andresmosquera739@gmail.com>
2024-08-27 09:58:40 -0500

    Functional dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { InventariosComponent } from './components/inventarios/inventarios.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'ventas', component: VentasComponent },
+      { path: 'inventarios', component: InventariosComponent },
+      { path: 'proveedores', component: ProveedoresComponent },
+      { path: 'clientes', component: ClientesComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { InventariosComponent } from './components/inventarios/inventarios.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    VentasComponent,
+    InventariosComponent,
+    ProveedoresComponent,
+    ClientesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
